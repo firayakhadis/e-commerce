291ec898693ea01b93e436b5ec1e738485d2aa0d
firayakhadis <112853822+firayakhadis@users.noreply.github.com>
2022-09-22 12:18:23 +0300

    changes

--- store/views.py
+++ store/views.py
@@ -3,11 +3,6 @@ from django.shortcuts import get_object_or_404, render
 from .models import Category, Product
 
 
-def categories(request):
-    return {
-        'categories': Category.objects.all()
-    }
-
 
 def all_products(request):
     products = Product.products.all()
