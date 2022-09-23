0bd4e4fb3c0a2a62288f26180ad935841fb8c9fa
firayakhadis <112853822+firayakhadis@users.noreply.github.com>
2022-09-23 22:54:16 +0300

    changes

--- basket/basket.py
+++ basket/basket.py
@@ -13,3 +13,14 @@ class Basket():
         if 'skey' not in request.session:
             basket = self.session['skey'] = {}
         self.basket = basket
+
+    def add(self, product):
+        """
+        Adding and updating the users basket session data
+        """
+        product_id = product_id
+
+        if product_id not in self.basket:
+            self.basket[product_id] = {'price': str(product.price)}
+
+        self.session.modified = True
