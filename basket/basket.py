d9ee9d3458f67b42a42409845875b0c8d9e64064
firayakhadis <112853822+firayakhadis@users.noreply.github.com>
2022-09-23 23:12:23 +0300

    changes

--- basket/basket.py
+++ basket/basket.py
@@ -21,6 +21,12 @@ class Basket():
         product_id = product_id
 
         if product_id not in self.basket:
-            self.basket[product_id] = {'price': str(product.price), 'qty':int(qty)}
+            self.basket[product_id] = {'price': str(product.price), 'qty': int(qty)}
 
         self.session.modified = True
+
+    def __len__(self):
+        """
+        Get the basket data and count the qty of items
+        """
+        return sum(item['qty'] for item in self.basket.values())
